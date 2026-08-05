9f88b09a07531f8952a52e9c64c142390398fe53
Semere Meharena Mebrahtom <semere.meharena1@gmail.com>
2026-08-05 12:47:41 +0200

    removed dwl

--- modules/default.nix
+++ modules/default.nix
@@ -27,8 +27,6 @@ allModules
       self.nixosModules.helix
       self.nixosModules.zen-browser
       self.nixosModules.niri
-      # self.nixosModules.noctalia
-      # self.nixosModules.dwl
       self.nixosModules.fuzzel
       self.nixosModules.fnott
       self.nixosModules.swaylock
